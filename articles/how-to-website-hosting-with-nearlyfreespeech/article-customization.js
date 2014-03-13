d52f737a4dca21654570395f3bd4d85b7a6ec2bf
Jean-Philippe Paradis <hexstream@gmail.com>
2014-03-13 00:58:10 -0400

    This article will feature optional customization options requiring javascript.

---
+++ articles/how-to-website-hosting-with-nearlyfreespeech/article-customization.js
@@ -0,0 +1,7 @@
+var js_enabled_elements;
+
+function init () {
+    js_enabled_elements = document.getElementsByClassName("javascript-is-enabled");
+
+    document.createTextNode("✔ Javascript is enabled.");
+}
